490d9d79c5c4ade988e214030f0f2dcebe1e3676
Aiden Appleby <aiden.appleby3@gmail.com>
2022-01-06 13:25:09 +0100

    added prod vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  GoogleClientID: '416360443439-vvmqadp7ehr8n07jabq6n6r3s896ifv1.apps.googleusercontent.com',
+  API_URL: 'https://api.hillview.tv/auth/v1.1'
 };
