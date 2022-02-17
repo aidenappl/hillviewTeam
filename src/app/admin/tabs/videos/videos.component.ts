520f38e5dbd0c77abdc0f9ed2f45e10ca3bc698a
Aiden Appleby <aiden.appleby3@gmail.com>
2022-02-16 17:20:42 -0800

    added thumbnail uploading

--- src/app/admin/tabs/videos/videos.component.ts
+++ src/app/admin/tabs/videos/videos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as dayjs from 'dayjs';
 import { environment } from 'src/environments/environment';
@@ -10,7 +11,7 @@ import { RequestService } from 'src/services/http/request.service';
     styleUrls: ['./videos.component.scss'],
 })
 export class VideosComponent implements OnInit {
-    constructor(private request: RequestService) {}
+    constructor(private request: RequestService, private http: HttpClient) {}
 
     @ViewChild('titleInput', { static: false }) titleInput!: ElementRef;
     @ViewChild('descriptionInput', { static: false }) descriptionInput!: ElementRef;
@@ -56,8 +57,40 @@ export class VideosComponent implements OnInit {
         }
     }
 
-    handleThumbnailUpload(): void {
+    clickThumbnailer(): void {
       this.thumbnailUploader.nativeElement.click()
+    }
+
+    thumbnailUpload(event: any): void {
+      var files = event.target!.files
+      var filename = files[0].name
+      var extension = files[0].type
+      
+      if (files.length === 0) {
+        window.alert("No file selected.");
+        return;
+      }
+  
+      if (files.length > 1) {
+        window.alert("Can't select more than one file");
+        return;
+      }
+  
+      console.log(files, filename, extension)
+  
+      const formData = new FormData();
+      formData.append('upload', files[0]);
+  
+      this.http.post<any>(`${environment.VIDEO_API_URL}/upload/thumbnail`, formData).subscribe(
+        (res: any) => {
+          console.log(res)
+          if (res.url) {
+            this.thumbnailInput.nativeElement.value = res.url
+            window.alert("Successfully Uploaded thumbnail!")
+          }
+        },
+        (err: any) => console.log(err)
+      );
     }
 
     async formatVideos(): Promise<void> {
