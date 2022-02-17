7e5fabd4e6d4d6cc4c61be40230de2d3887c90d9
Aiden Appleby <aiden.appleby3@gmail.com>
2022-02-17 14:04:53 -0800

    changed uploader design

--- src/app/admin/tools/video-uploader/video-uploader.component.ts
+++ src/app/admin/tools/video-uploader/video-uploader.component.ts
@@ -1,152 +1,235 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { RequestService } from 'src/services/http/request.service';
 
-import * as captureVideoFrame from "capture-video-frame";
+import * as captureVideoFrame from 'capture-video-frame';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-video-uploader',
-  templateUrl: './video-uploader.component.html',
-  styleUrls: ['./video-uploader.component.scss']
+    selector: 'app-video-uploader',
+    templateUrl: './video-uploader.component.html',
+    styleUrls: ['./video-uploader.component.scss'],
 })
 export class VideoUploaderComponent implements OnInit {
+    @ViewChild('uploadVideoInput', { static: false })
+    uploadVideoInput!: ElementRef;
+    @ViewChild('titleInput', { static: false }) titleInput!: ElementRef;
+    @ViewChild('descriptionInput', { static: false })
+    descriptionInput!: ElementRef;
+    @ViewChild('thumbnailInput', { static: false }) thumbnailInput!: ElementRef;
 
-  @ViewChild('uploadVideoInput', {static: false}) uploadVideoInput!: ElementRef;
-  @ViewChild('titleInput', {static: false}) titleInput!: ElementRef;
-  @ViewChild('descriptionInput', {static: false}) descriptionInput!: ElementRef;
+    showThumbnailer: boolean = false;
+    uploadInProgress: boolean = false;
 
-  showFileLoader: boolean = false;
-  showLoader: boolean = false;
-  fileNameTag: string = '';
-  videoSubmitButtonVal: string = 'Submit';
-  fileLink: string = '';
+    videoSubmitButtonVal: string = 'Submit';
 
-  constructor(
-    private http: HttpClient,
-    private request: RequestService
-  ) { }
+    thumbnailSubmitBtn: string = 'Select Frame As Thumbnail';
 
-  ngOnInit(): void {
-  }
+    data: any = {
+        title: '',
+        description: '',
+        url: '',
+        thumbnail: '',
+        localBlob: '',
+        uploadedFileName: '',
+        progress: 0,
+    };
 
-  fileUploadChange(event: any): void {
-    var files = event.target.files
-    var filename = files[0].name
-    var extension = files[0].type
-    
-    if (files.length === 0) {
-      window.alert("No file selected.");
-      this.showFileLoader = false;
-      return;
+    bools: any = {
+        hasUpload: false,
+        uploadFinished: false,
+        showProgress: false,
+        showfs: false,
+    };
+
+    constructor(
+        private http: HttpClient,
+        private router: Router,
+        private request: RequestService
+    ) {}
+
+    ngOnInit(): void {}
+
+    back(): void {
+        this.router.navigateByUrl('/admin/r/videos');
     }
 
-    if (files.length > 1) {
-      window.alert("Can't select more than one file");
-      this.showFileLoader = false;
-      return;
+    toggleThumbnailer(state: boolean): void {
+        this.showThumbnailer = state;
     }
 
-    console.log(files, filename, extension)
+    valueChange(target: string, event: any): void {
+        this.data[target] = event.target.value;
+    }
 
-    this.fileNameTag = "Uploading... " + filename
+    async fileUploadChange(event: any): Promise<void> {
+        try {
+            this.bools.hasUpload = true;
 
-    this.videoSubmitButtonVal = 'Uploading...'
+            var files = event.target.files;
+            var filename = files[0].name;
+            var extension = files[0].type;
 
+            if (files.length === 0) {
+                window.alert('No file selected.');
+                this.bools.hasUpload = false;
+                return;
+            }
 
-    const formData = new FormData();
-    formData.append('upload', files[0]);
+            if (files.length > 1) {
+                window.alert("Can't select more than one file");
+                this.bools.hasUpload = false;
+                return;
+            }
 
-    this.http.post<any>(`${environment.VIDEO_API_URL}/upload/video`, formData).subscribe(
-      (res) => {
-        console.log(res)
-        if (res.url) {
-          this.fileLink = res.url
-          this.showFileLoader = false;
-          this.videoSubmitButtonVal = 'Submit'
-          this.fileNameTag = "Uploaded! " + filename
+            console.log(files, filename, extension);
+
+            this.data.uploadedFileName = 'Uploading... ' + filename;
+
+            this.videoSubmitButtonVal = 'Uploading...';
+
+            this.bools.showProgress = true;
+            this.bools.showfs = true;
+
+            const formData = new FormData();
+            formData.append('upload', files[0]);
+
+            const b64 = await this.getFileURL(files[0]);
+
+            var video: any = document.getElementById('preview-video');
+            video.src = b64;
+            var thvideo: any = document.getElementById('thumbnailer-video');
+            thvideo.src = b64;
+
+            this.http
+                .post<any>(
+                    `${environment.VIDEO_API_URL}/upload/video`,
+                    formData,
+                    {
+                        reportProgress: true,
+                        observe: 'events',
+                    }
+                )
+                .subscribe((resp) => {
+                    if (resp.type === HttpEventType.Response) {
+                        this.bools.uploadFinished = true;
+                        this.bools.showProgress = false;
+                        this.bools.showfs = false;
+                        if (resp.body.url) {
+                            this.data.url = resp.body.url;
+                            this.videoSubmitButtonVal = 'Submit';
+                            this.data.uploadedFileName =
+                                'Uploaded! ' + filename;
+                        }
+                    }
+                    if (resp.type === HttpEventType.UploadProgress) {
+                        const percentDone = Math.round(
+                            (100 * resp.loaded) / resp.total!
+                        );
+                        this.data.progress = percentDone;
+                    }
+                });
+        } catch (error) {
+            console.error(error);
         }
-      },
-      (err) => console.log(err)
-    );
-  }
+    }
 
-  async submit(): Promise<void> {
-    try {
-      if (this.showFileLoader || this.showLoader) {return}
-      const data = {
-        url: this.fileLink,
-        title: this.titleInput.nativeElement.value,
-        description: this.descriptionInput.nativeElement.value,
-        thumbnail: "https://content.hillview.tv/thumbnails/default.jpg"
+    async getFileURL(file: any): Promise<any> {
+      try {
+        return URL.createObjectURL(file)
+      } catch (error) {
+        throw error
       }
-
-      this.showLoader = true;
-      this.videoSubmitButtonVal = ' '
-
-      const response = await this.request.post(`${environment.VIDEO_API_URL}/create/video`, data)
-      console.log(response)
-      if (response.status === 200) {
-        window.alert("Successfully uploaded video!")
-        window.location.reload()
-      } else {
-        window.alert("Something went wrong!" + (response.body as any).toString())
-        window.location.reload()
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  uploadFilePress(): void {
-    if (this.showFileLoader) {return}
-    this.showFileLoader = true;
-    this.uploadVideoInput.nativeElement.click();
-  }
-
-  getVideoFrame(): void {
-    try {
-      const frame = this.captureVideoFrame("preview-video", "jpeg", 0.92);
-      console.log(frame)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  captureVideoFrame(video: any, format: any, quality: any) {
-    if (typeof video === 'string') {
-        video = document.getElementById(video);
     }
 
-    format = format || 'jpeg';
-    quality = quality || 0.92;
-
-    if (!video || (format !== 'png' && format !== 'jpeg')) {
-      console
-        return false;
+    getBase64(file: any): Promise<any> {
+      const reader = new FileReader()
+      return new Promise(resolve => {
+        reader.onload = (ev: any) => {
+          resolve(ev.target.result)
+        }
+        reader.readAsDataURL(file)
+      })
     }
 
-    var canvas: any = document.createElement("CANVAS");
+    async submit(): Promise<void> {
+        try {
+            const data = {
+                url: this.data.url,
+                title: this.titleInput.nativeElement.value,
+                description: this.descriptionInput.nativeElement.value,
+                thumbnail:
+                    this.data.thumbnail.length > 0
+                        ? this.data.thumbnail
+                        : 'https://content.hillview.tv/thumbnails/default.jpg',
+            };
 
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    canvas.getContext('2d').drawImage(video, 0, 0);
-
-    var dataUri = canvas.toDataURL('image/' + format, quality);
-    var data = dataUri.split(',')[1];
-    var mimeType = dataUri.split(';')[0].slice(5)
-
-    var bytes = window.atob(data);
-    var buf = new ArrayBuffer(bytes.length);
-    var arr = new Uint8Array(buf);
-
-    for (var i = 0; i < bytes.length; i++) {
-        arr[i] = bytes.charCodeAt(i);
+            const response = await this.request.post(
+                `${environment.VIDEO_API_URL}/create/video`,
+                data
+            );
+            console.log(response);
+            if (response.status === 200) {
+                window.alert('Successfully uploaded video!');
+                window.location.reload();
+            } else {
+                window.alert(
+                    'Something went wrong!' + (response.body as any).toString()
+                );
+                window.location.reload();
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    var blob = new Blob([ arr ], { type: mimeType });
-    return { blob: blob, dataUri: dataUri, format: format };
-};
+    uploadFilePress(): void {
+        if (this.bools.hasUpload) {
+            return;
+        }
+        this.uploadVideoInput.nativeElement.click();
+    }
 
+    async getVideoFrame(): Promise<void> {
+        try {
+            this.thumbnailSubmitBtn = 'Generating...';
+            const frame: any = captureVideoFrame(
+                'thumbnailer-video',
+                'jpeg',
+                0.5
+            );
+            this.thumbnailSubmitBtn = 'Uploading...';
+
+            const formData = new FormData();
+            formData.append('upload', frame.blob);
+
+            this.http
+                .post<any>(
+                    `${environment.VIDEO_API_URL}/upload/thumbnail`,
+                    formData,
+                    {
+                        reportProgress: true,
+                        observe: 'events',
+                    }
+                )
+                .subscribe((resp) => {
+                    if (resp.type === HttpEventType.Response) {
+                        this.thumbnailSubmitBtn = 'Done!';
+                        this.toggleThumbnailer(false);
+                        this.data.thumbnail = resp.body.url;
+                        this.thumbnailInput.nativeElement.value = resp.body.url;
+                        window.alert('Successfully uploaded thumbnail!');
+                    }
+                    if (resp.type === HttpEventType.UploadProgress) {
+                        const percentDone = Math.round(
+                            (100 * resp.loaded) / resp.total!
+                        );
+                        console.log('Progress ' + percentDone + '%');
+                    }
+                });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
